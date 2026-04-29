e0d198451c73afc92b1d0448047afd9e9f95dfda
Mykhailo_Zinoviev <bestmzua@gmail.com>
2026-04-29 18:20:34 +0300

    inital commit

---
+++ recorder.py
@@ -0,0 +1,28 @@
+import subprocess
+import os
+
+ffmpeg_path = r"D:\ffmpeg\ffmpeg.exe"
+
+# 📁 Папка для сохранения
+save_folder = r"D:\Recordings"
+
+# создаём папку если её нет
+os.makedirs(save_folder, exist_ok=True)
+
+# имя файла
+output_file = os.path.join(save_folder, "record.mp4")
+
+command = [
+    ffmpeg_path,
+    "-f", "gdigrab",
+    "-framerate", "60",
+    "-video_size", "1920x1080",
+    "-i", "desktop",
+    "-c:v", "h264_nvenc",
+    "-preset", "p5",
+    "-b:v", "20M",
+    "-pix_fmt", "yuv420p",
+    output_file
+]
+
+subprocess.run(command)
